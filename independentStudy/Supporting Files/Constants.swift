686faa43b9888edbcb79c3f9f2d19b75d4716404
RobertWais <wais.robert@uwlax.edu>
2018-07-11 23:40:01 -0700

    Cleaning up strings and replacing with constants

---
+++ independentStudy/Supporting Files/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  independentStudy
+//
+//  Created by Robert Wais on 7/11/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+import Foundation
+
+
+struct Constants {
+    struct Segue {
+        static let showSection = "show"
+    }
+    
+}
